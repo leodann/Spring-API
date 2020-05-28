5eb8bb2009f17ed2a2815d43e4a8c8b076624989
legokna <leo_dan2010@hotmail.com>
2020-05-28 11:34:30 -0500

    inicio de proyecto

---
+++ src/main/java/com/tapweb/srpingapi/SrpingapiApplication.java
@@ -0,0 +1,13 @@
+package com.tapweb.srpingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tapweb/srpingapi/SrpingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapweb.srpingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
